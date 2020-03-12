a4471020d82fa05307e705d58380eaf2fdcb28b7
Hank.Leeeee <hank.leeeee@kakaocorp.com>
2020-03-12 22:25:06 +0900

    Initial Commit

---
+++ SwiftUtility/SwiftUtility.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtility.h
+//  SwiftUtility
+//
+//  Created by Hank.Lee on 12/03/2020.
+//  Copyright © 2020 hyeongkyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUtility.
+FOUNDATION_EXPORT double SwiftUtilityVersionNumber;
+
+//! Project version string for SwiftUtility.
+FOUNDATION_EXPORT const unsigned char SwiftUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtility/PublicHeader.h>
+
+
