a9c10344d635cdea37ce2968729d1bb05f53f45c
Hank.Leeeee <hank.leeeee@kakaocorp.com>
2020-04-14 01:33:49 +0900

    no message

--- SwiftUtility/SequenceUtility.swift
+++ SwiftUtility/SequenceUtility.swift
@@ -1,5 +1,5 @@
 //
-//  ArrayUtility.swift
+//  SequenceUtility.swift
 //  SwiftUtility
 //
 //  Created by Hank.Lee on 14/03/2020.
@@ -15,5 +15,12 @@ public extension Array {
     
     var isCompactEmpty: Bool {
         return self.compactMap({ $0 }).isEmpty
+    }
+}
+
+public extension Sequence where Element: Hashable {
+    func toSet<Element>() -> Set<Element> {
+        // swiftlint:disable force_cast
+        return Set(self) as! Set<Element>
     }
 }
